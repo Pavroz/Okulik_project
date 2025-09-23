bdaa32a27e78c936a705415389d5f9814ef1d884
Main <fyriones@yandex.ru>
2025-09-23 23:20:36 +0500

    lesson_8 commit

--- lesson_8/lesson_8.py
+++ lesson_8/lesson_8.py
@@ -1,1 +1,12 @@
 import random
+import sys
+
+print(sys.platform)
+
+print(random.random())
+print(int(random.random() * 100))
+print(random.randint(0 ,100))
+print(random.randrange(0,100, 2))
+
+users = ['user1', 'user2', 'user3']
+print(random.choice(users))
