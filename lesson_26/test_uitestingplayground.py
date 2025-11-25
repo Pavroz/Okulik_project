c6bb070f5846e651192d981ab10cd0ea9cf15f2c
Pavroz <fyriones@yandex.ru>
2025-11-25 23:06:37 +0500

    qwe

--- lesson_26/test_uitestingplayground.py
+++ lesson_26/test_uitestingplayground.py
@@ -85,3 +85,28 @@ class TestUi:
         expect(page.locator("#transparentButton")).to_be_visible()  # opacity: 0
         expect(page.locator("#overlappedButton")).to_be_visible()  # перекрыта
         expect(page.locator("#offscreenButton")).to_be_visible()  # сдвинута влево на -100px
+        
+    def test_sample_app(self, page: Page, base_page, open):
+        name = 'test'
+        page.locator('a[href="/sampleapp"]').click()
+        page.locator('input[name="UserName"]').fill(f'{name}')
+        page.locator('input[name="Password"]').fill('pwd')
+        page.locator('#login').click()
+        expect(page.locator('#loginstatus')).to_have_text(f'Welcome, {name}!')
+        page.get_by_text('Log Out').click()
+        expect(page.locator('#loginstatus')).to_have_text('User logged out.')
+
+    def test_mouse_over(self, page: Page, base_page, open):
+        page.get_by_role('link', name='Mouse Over').click()
+        page.locator('a[title="Click me"]').hover()
+        page.locator('a[title="Active Link"]').click()
+        page.locator('a[title="Active Link"]').click()
+        expect(page.locator('#clickCount')).to_have_text('2')
+        page.locator('a[title="Link Button"]').dblclick()
+        expect(page.locator('#clickButtonCount')).to_have_text('2')
+
+    def test_non_breaking_space(self, page: Page, base_page, open):
+        page.get_by_role('link', name='Non-Breaking Space').click()
+        page.get_by_role('button', name='My\u00A0Button').click()
+        # \u00A0 - символ неразрывного пробела, если в html коде '&nbsp;'
+        # Пример: <button class="btn btn-primary" type="button">My&nbsp;Button</button>
